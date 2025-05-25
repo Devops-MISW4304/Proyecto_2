2fb196a34090fddab6a7927b7e68e63e7e05cf08
JohanBautistaQ <j.bautistaq@uniandes.edu.co>
2025-05-25 09:20:32 -0500

    deploy

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 @app.cli.command('init_db')
 def init_db_command():
     """Crea las tablas de la base de datos."""
